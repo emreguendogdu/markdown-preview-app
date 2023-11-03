9b5984ff56fb19bca3187f2e681e96f3349cc20e
Osman Emre Gundogdu <emreguendogdu@yandex.com>
2023-11-03 17:20:28 +0300

    Exported placeholder to its own component

---
+++ src/Placeholder.js
@@ -0,0 +1,44 @@
+export const placeholder = `# Welcome to React Markdown Previewer!
+
+### And here's some other cool stuff:
+
+Heres some inline code; \`<div></div>\` between 2 backticks.
+
+\`\`\`javascript
+// or a multi-line code
+
+function sayHi(word) {
+  if (word.toLowerCase() === "hi" || word.toLowerCase() === "hello") {
+    return true;
+  }
+}
+\`\`\`
+
+You can also make text **bold**... whoa!
+Or _italic_.
+Or... wait for it... **_both!_**
+And feel free to go crazy ~~crossing stuff out~~.
+
+There's also [links](https://www.freecodecamp.org), and
+ > "The two most powerful warriors are patience and time." - Leo Tolstoy
+
+And if you want to get really crazy, even tables:
+
+Wild Header | Crazy Header | Another Header?
+------------ | ------------- | -------------
+Your content can | be here, and it | can be here....
+And here. | Okay. | I think we get it.
+
+- And of course there are lists.
+  - Some are bulleted.
+     - With different indentation levels.
+        - That look like this.
+
+
+1. And there are numbered lists too.
+1. Use just 1s if you want!
+
+ # Embedded images
+
+![William Blake](https://cryrzzvxza.cloudimg.io/rybikyem.beget.tech/painters/72/5.jpg)
+`
