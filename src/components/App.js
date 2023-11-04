3683ae7f416fd6eec95d69134872af190ce96814
Osman Emre Gundogdu <emreguendogdu@yandex.com>
2023-11-04 06:49:43 +0300

    Increased readability

--- src/components/App.js
+++ src/components/App.js
@@ -3,28 +3,27 @@ import '../styles/App.scss';
 import Preview from './Preview';
 import { placeholder } from "./Placeholder";
 
-const App = () => {
-    const [value, setValue] = useState(placeholder);
+function App() {
+  const [value, setValue] = useState(placeholder);
 
-    const handleChange = (e) => {
+  const handleChange = (e) => {
     setValue(e.target.value);
   };
 
-  console.log(value);
-    return (
+  return (
     <>
-    {/* Make all hyperlinks open in a new tab */}
-    <head>
-      <base target="_blank" /> 
-    </head>
+      {/* Make all hyperlinks open in a new tab */}
+      <head>
+        <base target="_blank" />
+      </head>
 
       <body>
-        <textarea id="editor" value={value} onChange={handleChange}></textarea>
-        <Preview value={value}/>
+        <textarea id="editor" value={value} onChange={handleChange} />
+        <Preview value={value} />
       </body>
     </>
   );
-};
+}
 
   
 
